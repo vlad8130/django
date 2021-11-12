b2ee29ee4beac3fc2241560bff4985e75b9ab065
vlad8130 <vlisitskiy@gmail.com>
2021-11-12 11:25:00 +0200

    HW 1,2

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import index, first
-
-
+from .views import index, first, articles, article_number, archive_num, phone_number, users, users_num, archive, \
+    code
 
 urlpatterns = [
     path('', index, name='index'),
-path('first', first, name='first'),
+    path('first', first, name='first'),
+    path('articles/', articles, name='articles'),
+    path('article/<int:article_id>/archive/', archive_num, name='archive_num'),
+    path('articles/<int:article_id>/', article_number, name='article_number'),
+    path('articles/<int:article_id>/<slug:name>/', article_number, name='article_name'),
+    path('articles/archive/', archive, name='archive'),
+    path('users/', users, name='users'),
+    path('users/<int:user_number>/', users_num, name='users_num'),
+    path('[0-9]{10}', phone_number, name='phone_number'),
+    re_path('[0-9a-f]{4}[-][0-9a-f]{6}', code, name='code'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-# from django.http import HttpResponse
+import re
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -26,13 +28,54 @@ def index(request):
         'my_set': my_set,
         'my_tuple': my_tuple,
         'my_class': my_class,
-
     })
 
 
 def first(request):
     name = 'Vladimir'
-
     return render(request, 'first.html', {
         'name': name,
     })
+
+
+def articles(request):
+    return render(request, 'articles.html')
+
+
+def archive(request):
+    return render(request, 'archive.html')
+
+
+def users(request):
+    return render(request, 'users.html')
+
+
+def article_number(request, article_id, name=''):
+
+    data = {'article_id': article_id, 'name': name}
+    return render(request, 'article_number.html', {
+        'check': article_id % 2,
+        'slug': name
+        })
+
+
+def archive_num(request, article_id):
+    return HttpResponse('{} archive Success!'.format(article_id))
+
+
+def users_num(request, user_number):
+    return HttpResponse('User {} Success!'.format(user_number))
+
+
+def phone_number(request, phone_num):
+    code_list = ['067', '068', '096', '097', '098', '050', '066', '095', '099', '063', '093', '091', '092', '094']
+    if len(str(phone_num)) == 10:
+        for i in code_list:
+            result = re.match(i, str(phone_num))
+            if result:
+                return HttpResponse("Ukrainian phone number")
+    return HttpResponse("Number is not valid")
+
+
+def code(request):
+    return HttpResponse('Your code is converted!')
